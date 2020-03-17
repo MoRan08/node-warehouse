bc4869784c79cdbced385124c493d8c5d461672e
MoRan08 <1173395669@qq.com>
2020-03-17 18:01:29 +0800

    admin

--- controller/admin/admin.js
+++ controller/admin/admin.js
@@ -1,1 +1,72 @@
 'use strict';
+
+import AdminModel from '../../models/admin/admin'
+import crypto from 'crypto'
+import formidable from 'formidable'
+
+class Admin {
+    constructor() {
+        super()
+        this.login = this.login.bind(this)
+        this.register = this.register.bind(this)
+    }
+    //登录
+    async login(req, res, next) {
+        const form = new formidable.IncomingForm();
+        form.parse(req, async (err, fields, files) => {
+            if (err) {
+                res.send({
+                    status: 0,
+                    type: 'FORM_DATA_ERROR',
+                    message: '表单信息错误'
+                })
+                return
+            }
+            const {user_name, password, status = 1 } = fields;
+            try {
+                if (!user_name) {
+                    throw new Error('用户名参数错误')
+                } else if (!password) {
+                    throw new Error('密码参数错误')
+                }
+            } catch (err) {
+                console.log(err.message, err);
+                res.send({
+                    status: 0,
+                    type: 'GET_ERROR_PARAM',
+                    message: err.message,
+                })
+                return
+            }
+            const newpassword = this.encryption(password);//密码基于Md5加密
+            try{
+                const admin = await AdminModel.findOne({user_name})
+                if(!admin){
+                    const adminTip = status==1?'管理员':'超级管理员'
+                    const newAdmin ={
+                        user_name,
+                        password:newpassword,
+                        
+                    }
+                }
+            }catch(err){
+
+            }
+        })
+
+    }
+    //注册
+    async register(req, res, next) {
+
+    }
+    //生成新密码
+    encryption(password){
+        const newpassword = this.Md5(this.Md5(password).substr(2,7))+this.Md5(password);
+        return newpassword
+    }
+    //密码MD5加密
+    Md5(password){
+        const md5 = crypto.createHash('md5');
+        return md5.update(password).digest('base64');
+    }
+}
--- models/admin/admin.js
+++ models/admin/admin.js
@@ -5,9 +5,9 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const adminSchema = new Schema({
-    user_name:String,
-    password:String,
-    id:Number,
+    user_name: String,
+    password: String,
+    id: Number,
     create_time: String,
     admin: {
         type: String,
@@ -19,12 +19,38 @@ const adminSchema = new Schema({
         default: 'default.jpg'
     },
     city: String,
-    gender:Number,//1:男性、2：女性
-    call_phone:Number,
+    gender: Number, //1:男性、2：女性
+    call_phone: Number,
 });
 
-adminSchema.index({id:1});
+adminSchema.index({
+    id: 1
+});
 
-const Admin = mongoose.model('Admin',adminSchema);
+const Admin = mongoose.model('Admin', adminSchema);
+
+// 插入初始化数据一条
+//根据模型创建实体，是指的个体对象
+// var personEntity = new Admin({
+//     user_name: 'admin',
+//     password: '123456',
+//     id: 1,
+//     create_time: '2020/3/17 12:12:00',
+//     admin: '',
+//     status: 2, //1:普通管理、 2:超级管理员
+//     avatar: '',
+//     city: '山东临沂',
+//     gender: 1, //1:男性、2：女性
+//     call_phone: 18769977818,
+// });
+// //用save 方法把自己保存到数据库中
+// personEntity.save(function (error, doc) {
+//     if (error) {
+//         console.log("error :" + error);
+//     } else {
+//         console.log(doc);
+//     }
+// });
+
 
 export default Admin
